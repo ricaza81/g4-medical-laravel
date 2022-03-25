d34ce9f1ae3419f103535555e66cc9d879e0d4ae
ricaza81 <ricaza81@gmail.com>
2022-03-24 22:47:09 -0500

    ajustes

--- resources/views/correo/plantilla_garantia.blade.php
+++ resources/views/correo/plantilla_garantia.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Activación de Garantía</title>
-       <style>
-       .titulo {
+    <style>
+        .titulo {
         color: #1e80b6;
         padding-top: 20px;
         padding-bottom: 10px;
@@ -28,46 +28,8 @@
        </style>
 </head> 
 <body class="body">
-<p style="margin-left:100px;margin-top:-70px;heigth:137px;width:452px;margin-bottom:-200px">
-    <img style="margin-left:10px;margin-top:-60px;width:437px;heigth:137px;padding-bottom:40px" src="https://lifenmedical.com/garantias/wp-content/uploads/2022/02/tcf.png"/>
-</p>
-<div class="row" style="margin-top:-100px">
-    <div class="col-md-3" style="float:left;width:400px;margin-top:10px;margin-left:100px;padding-top:30px">
-            <p style="padding-top:20px;margin-left:100px">
-                refcolor
-                <span style="margin-left:-420px">
-                    <img style="margin-left:-180px;margin-top:-10px;width:298px;heigth:53px" src="https://lifenmedical.com/garantias/wp-content/uploads/2022/03/line-4-referencia.png"/>
-                    </span>
-            </p>
-    </div>    
-    <div class="col-md-2">
-            <img style="margin-left:-290px;margin-top:10px;width:150px;heigth:150px" src="https://lifenmedical.com/garantias/wp-content/uploads/2022/02/lenz-logo.png"/>
-    </div>
-    <div class="col-md-3" style="float:right;width:300px;margin-top:-201px">
-        <p style="padding-top:15px;margin-left:-20px;padding-right:320px;padding-bottom:16px">
-                [selct_multiselct-961]
-        </p>    
-        <p style="margin-top:-40px;margin-left:100px;padding-top:9px">
-        
-                    <img style="margin-left:-50px;width:298px;heigth:53px;padding-top:10px" src="https://lifenmedical.com/garantias/wp-content/uploads/2022/03/line-numero-chasis.png"/>
-        </p>            
-    </div>
-</div>
-<div class="row" style="margin-top:-195px;margin-left:-30px;text-align:center">
-    <p style="padding-right:60px">
-    Por el presente documento certificamos el registro de la garantía<br/> a nombre de:<br/><br/>
-        <h3>
-            text-595
-    </h3>
-        <img style="margin-left:15px;margin-top:-50px;width:298px;heigth:53px" src="https://lifenmedical.com/garantias/wp-content/uploads/2022/03/line-2.png"/>
-    </p>
-</div>
-<div class="row" style="margin-top:-50px;padding-left:15px;margin-left:-10px;padding-right:170px;text-align:center">
-<p style="width:350px;text-align:center;padding-right:170px">
-    desde la fecha establecida en este documento. En caso de cualquier consulta o reclamo no dude en comunicarse con nosotros para hacer efectiva la garantía.
-    Recuerde revisar las condiciones de garantía de por vida aplicables que se encuentran disponibles en nuestra página web https://lifenmedical.com/garantias/politica-de-garantia/
-    <img style="padding-top:15px;margin-left:15px;margin-top:-50px;width:427px;heigth:116px" src="https://lifenmedical.com/garantias/wp-content/uploads/2022/03/img-contacto.png"/> 
-</p>
+<div>
+    Hola
 </div>
 </body>
 </html>
